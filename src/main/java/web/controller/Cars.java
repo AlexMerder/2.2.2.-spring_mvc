d7a33decc772445d28311283ae48efc46c1971e8
AlexMerder <63518412+AlexMerder@users.noreply.github.com>
2021-02-28 14:57:46 +0300

    Исправлена ошибка, при которой при запросе /cars ничего

--- src/main/java/web/controller/Cars.java
+++ src/main/java/web/controller/Cars.java
@@ -1,15 +1,9 @@
 package web.controller;
 
 
-import com.google.common.io.LimitInputStream;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.ui.ModelMap;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -20,7 +14,7 @@ import java.util.List;
 public class Cars {
 
     @GetMapping(value = "/cars")
-    public String countCars(@RequestParam(value = "count", required = false) int count, Model model) {
+    public String countCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("bmv", 170, 150));
         cars.add(new Car("oka", 25, 22));
@@ -28,17 +22,22 @@ public class Cars {
         cars.add(new Car("porshe", 190, 333));
         cars.add(new Car("lada", 110, 30));
 
-//        List<Car> result;
-//        result = new ArrayList<>(cars.subList(0, count));
-        List<Car> result = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            if (count >= 5) {
-                result.addAll(cars.subList(0, cars.size()));
-                break;
-            } else
-                result.add(cars.get(i));
+        if (count == null) {
+            List<Car> resultNull = new ArrayList<>(cars.subList(0, cars.size()));
+            model.addAttribute("count", resultNull);
+        } else {
+            List<Car> result = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                if (count >= 5) {
+                    result.addAll(cars.subList(0, cars.size()));
+                    break;
+                } else
+                    result.add(cars.get(i));
+            }
+            model.addAttribute("count", result);
         }
-        model.addAttribute("count", result);
+
+
         return "cars";
     }
 
